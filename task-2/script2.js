ef71cbb50f33844bc17bc8cdc890dc2bdcb09d43
Pavel Yermakov <pavelak137@mail.ru>
2021-12-02 23:32:17 +0300

    fix task

--- task-2/script2.js
+++ task-2/script2.js
@@ -6,6 +6,17 @@ if (age1 > 0 && !isNaN(age1)) {
   } else {
     console.log(`Привет мне ${age1} лет`);
   }
+
+  switch (true) {
+    case age <= 18:
+      console.log(`Привет мне ${age1} лет`);
+      break;
+    case age > 18:
+      console.log(`Здравствуйте, мне ${age1} лет`);
+      break;
+    default:
+      console.log("Неверно указан возраст");
+  }
 } else {
   console.log("Неверно указан возраст");
 }
