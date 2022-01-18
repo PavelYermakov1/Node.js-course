9c022703e1a3305369dd0152c0e2a8e222b68775
Pavel Yermakov <pavelak137@mail.ru>
2022-01-18 19:11:25 +0300

    add task 11

---
+++ script.js
@@ -0,0 +1,8 @@
+function HouseBuilder(length, width, floors) {
+  this.length = length;
+  this.width = width;
+  this.floors = floors;
+}
+
+const firstHouse = new HouseBuilder(20, 40, 80);
+const secondHouse = new HouseBuilder(30, 60, 90);
