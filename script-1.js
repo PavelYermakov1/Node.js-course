0e2637a12262378aa9591d14b0371e90f71c0ebd
Pavel Yermakov <pavelak137@mail.ru>
2022-01-21 21:51:25 +0300

    fix

--- script-1.js
+++ script-1.js
@@ -4,14 +4,3 @@ function invert(data) {
   }
   data.split("").reverse().join("");
 }
-
-function invertOptimized(data) {
-  const array = [...data];
-  for (let i = 0; i < (array.length - 1) / 2; i++) {
-    const temp = array[i];
-    array[i] = array[array.length - 1 - i];
-    array[array.length - 1 - i] = temp;
-  }
-
-  return Array.isArray(data) ? array : array.join("");
-}
