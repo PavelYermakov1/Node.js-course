c08cc5ebe74f6cc3cb8b727da066543f7d35781a
Pavel Yermakov <pavelak137@mail.ru>
2022-01-25 19:48:05 +0300

    task-13

--- script-1.js
+++ script-1.js
@@ -4,3 +4,14 @@ function invert(data) {
   }
   data.split("").reverse().join("");
 }
+
+function invertOptimized(data) {
+  const result = [...data];
+  const middleResult = Math.floor((result.length - 1) / 2);
+  for (let i = 0; i <= middleResult; i++) {
+    const temp = result[i];
+    result[i] = result[result.length - 1 - i];
+    result[result.length - 1 - i] = temp;
+  }
+  return Array.isArray(data) ? result : result.join("");
+}
